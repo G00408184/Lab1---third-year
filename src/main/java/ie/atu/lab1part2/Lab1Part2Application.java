fd4954f434dbcda7d5b9a7baeca60ad72668c365
Jakub Mamala <G00408184@atu.ie>
2023-10-03 15:53:57 +0100

    Created project

---
+++ src/main/java/ie/atu/lab1part2/Lab1Part2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.lab1part2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Part2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1Part2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab1part2/Lab1Part2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab1part2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1Part2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
