a15cabb59f31e94fe69ceefb69a4ee938eac3e80
Jakub Mamala <G00408184@atu.ie>
2023-10-03 16:29:21 +0100

    Added code into greetByName method

--- src/main/java/ie/atu/lab1part2/RequestController.java
+++ src/main/java/ie/atu/lab1part2/RequestController.java
@@ -1,6 +1,8 @@
 package ie.atu.lab1part2;
 
 import  org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import  org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +13,15 @@ public class RequestController {
         return "Hello World!";
     }
 
+    @GetMapping("/greet/{name}")
+    public  String greetByName(@PathVariable String name ){
+        return "Hello " + name;
+    }
+
+   // @GetMapping("/details")
+   // public  String details(@RequestParam int age ){
+    //    return "Hello " + age ;
+    //}
+
 }
+
