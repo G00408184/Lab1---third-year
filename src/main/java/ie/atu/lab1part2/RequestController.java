789ed4ff4d79782d9fda1a5555c98d11be9f9000
Jakub Mamala <G00408184@atu.ie>
2023-10-03 16:03:03 +0100

    Added code into RequestController class

---
+++ src/main/java/ie/atu/lab1part2/RequestController.java
@@ -0,0 +1,14 @@
+package ie.atu.lab1part2;
+
+import  org.springframework.web.bind.annotation.GetMapping;
+import  org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RequestController {
+
+    @GetMapping("/hello")
+    public  String hello() {
+        return "Hello World!";
+    }
+
+}
